44229b0174d1d59240b3503f9ba47d5ae077de6a
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-09-15 18:41:03 -0600

    Passing all test cases for chessMove. Probably highly unoptimized

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -7,23 +7,28 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+    private ChessPosition sPosition;
+    private ChessPosition ePosition;
+    private ChessPiece.PieceType promType;
 
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.sPosition = startPosition;
+        this.ePosition = endPosition;
+        this.promType = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return sPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return ePosition;
     }
 
     /**
@@ -33,6 +38,62 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promType;
+    }
+
+    @Override public String toString() {
+        String retString = sPosition.toString()+','+ePosition.toString()+',';
+        if (getPromotionPiece() == null) {
+            return retString+"NULL";
+        }
+        switch (getPromotionPiece()) {
+            case KING:                
+                return retString+"KING";
+            case QUEEN:
+                return retString+"QUEEN";
+            case BISHOP:
+                return retString+"BISHOP";
+            case KNIGHT:
+                return retString+"KNIGHT";
+            case ROOK:
+                return retString+"ROOK";
+            case PAWN:
+                return retString+"PAWN";
+            default:
+                return retString+"OOPSEY";
+        }
+    }
+
+    @Override public int hashCode() { //ChessMove hashCode is sPositionePositionpromType
+        int retValue = Integer.parseInt(Integer.toString(getStartPosition().hashCode())+Integer.toString(getEndPosition().hashCode()));
+        if (getPromotionPiece() == null) {
+            return Integer.parseInt(Integer.toString(retValue)+Integer.toString(0));
+        }
+        switch (getPromotionPiece()) {
+            case KING:                
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(1));
+            case QUEEN:
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(2));
+            case BISHOP:
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(3));
+            case KNIGHT:
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(4));
+            case ROOK:
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(5));
+            case PAWN:
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(6));
+            default:
+                return Integer.parseInt(Integer.toString(retValue)+Integer.toString(7));
+        }
+    }
+
+    @Override public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        ChessMove objComp = (ChessMove) obj;
+        return hashCode() == objComp.hashCode();
+        //try this again after implementing equals for chesspiece.piecetype
+        //return getStartPosition() == objComp.getStartPosition() && getEndPosition() == objComp.getEndPosition() && getPromotionPiece() == objComp.getPromotionPiece();
     }
 }
