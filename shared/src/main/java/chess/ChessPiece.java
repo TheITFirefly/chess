602758d1f0bde1ae2d542e82036dfa7bf912f4ae
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-10-17 22:23:08 -0600

    added braces to if statements

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -91,8 +91,12 @@ public class ChessPiece {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         ChessPiece that = (ChessPiece) o;
         return pColor == that.pColor && pType == that.pType;
     }
