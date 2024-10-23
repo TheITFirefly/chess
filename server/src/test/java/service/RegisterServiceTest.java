0e5cbeb6d3ce94f27e6cbfed8f6e49727aaff94c
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-10-22 23:09:22 -0600

    Positive RegisterService test

---
+++ server/src/test/java/service/RegisterServiceTest.java
@@ -0,0 +1,25 @@
+package service;
+
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import datatransfer.RegisterResponse;
+import datatransfer.DataTransfer;
+import datatransfer.RegisterRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class RegisterServiceTest {
+    @Test
+    @Order(1)
+    @DisplayName("Register user positive")
+    public void successfulUserRegistration() {
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        RegisterService service = new RegisterService(authDAO,userDAO);
+        RegisterRequest request = new RegisterRequest("lolcats","passw00rd","foo@bar.baz");
+        DataTransfer result = service.register(request);
+        Assertions.assertInstanceOf(RegisterResponse.class, result.data());
+    }
+}
