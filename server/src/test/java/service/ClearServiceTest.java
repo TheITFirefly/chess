a57723f4b8b863bfc1114c388ffa0a4ef811e3f1
TheITFirefly <hubprgm.lnemf@aleeas.com>
2024-10-31 22:42:02 -0600

    Added test for database implementation to ClearServiceTest

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,5 +1,6 @@
 package service;
 
+import dataaccess.DatabaseUserDAO;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
@@ -11,10 +12,23 @@ import org.junit.jupiter.api.*;
 public class ClearServiceTest {
     @Test
     @Order(1)
+    @DisplayName("Clear memory positive")
+    public void memoryClearPositive(){
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+        ClearService service = new ClearService(authDAO,userDAO,gameDAO);
+        DataTransfer result = service.clearDatabase();
+        Assertions.assertInstanceOf(ClearResponse.class, result.data());
+
+    }
+
+    @Test
+    @Order(2)
     @DisplayName("Clear database positive")
     public void databaseClearPositive(){
+        DatabaseUserDAO userDAO = new DatabaseUserDAO();
         MemoryAuthDAO authDAO = new MemoryAuthDAO();
-        MemoryUserDAO userDAO = new MemoryUserDAO();
         MemoryGameDAO gameDAO = new MemoryGameDAO();
         ClearService service = new ClearService(authDAO,userDAO,gameDAO);
         DataTransfer result = service.clearDatabase();
